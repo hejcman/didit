b12d71e867ecac84d66f13e5ff6e6a56994c4a08
Lukas Hejcman <lukas.hejcman@outlook.com>
2022-10-06 08:25:21 +0200

    Basic camera and DB implementation.
    * Taking pictures works as a proof of concept.
    * Saving pictures as blobs to the database works.
    * Database schema is implemented. AFAIK nothing more complicated will be needed for P1.
    * Recalling the image blobs from the DB and displaying them to the user implemented.

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,167 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-import 'memories.dart';
+import 'storage.dart';
 
-void main() => runApp(const CupertinoTabBarApp());
+/// The name of the DB in which the memories are stored.
+const String memoryBoxName = "memories";
 
-class CupertinoTabBarApp extends StatelessWidget {
-  const CupertinoTabBarApp({super.key});
+Future<void> main(List<String> args) async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  // Prepare the DB
+  await Hive.initFlutter();
+  generateAdapters();
+  await Hive.openBox<Memory>(memoryBoxName);
+
+  // Launch the app
+  runApp(MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        camera: firstCamera,
+      )));
+}
+
+// A screen that allows users to take a picture using a given camera.
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    super.key,
+    required this.camera,
+  });
+
+  final CameraDescription camera;
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      theme: CupertinoThemeData(brightness: Brightness.light),
-      home: CupertinoTabBarExample(),
+    return Scaffold(
+      // appBar: AppBar(title: const Text('Take a picture')),
+      // You must wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner until the
+      // controller has finished initializing.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Attempt to take a picture and get the file `image`
+            // where it was saved.
+            final image = await _controller.takePicture();
+
+            // Create the memory from the captured image.
+            final memory = Memory(await image.lastModified(),
+                await image.readAsBytes(), LifetimeTag.oneDay);
+
+            // Prepare connection to the DB so that we can save the image.
+            Box<Memory> memoryBox = Hive.box<Memory>(memoryBoxName);
+            // Save the image.
+            memoryBox.add(memory);
+
+            if (!mounted) return;
+
+            // If the picture was taken, display it on a new screen.
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  // Pass the automatically generated path to
+                  // the DisplayPictureScreen widget.
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
     );
   }
 }
 
-class CupertinoTabBarExample extends StatelessWidget {
-  const CupertinoTabBarExample({super.key});
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.collections),
-            label: 'Memories',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.list_number),
-            label: 'Lists',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.camera_fill),
-            label: 'Create',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.tag),
-            label: 'Tags',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.settings),
-            label: 'Settings'
-          )
-        ],
-      ),
-      tabBuilder: (BuildContext context, int index) {
-        switch (index) {
-          case 0:
-            return const MemoriesPage();
-          default:
-            break;
-        }
-        return CupertinoTabView(
-          builder: (BuildContext context) {
-            return Center(
-              child: Text('Content of tab $index'),
-            );
-          },
-        );
-      },
-    );
+    return Scaffold(
+        appBar: AppBar(title: const Text('Display the Picture')),
+        body: ValueListenableBuilder(
+            valueListenable: Hive.box<Memory>(memoryBoxName).listenable(),
+            builder: (BuildContext context, Box<Memory> box, _) {
+              // If there are no images, return info text.
+              if (box.values.isEmpty) {
+                return const Center(child: Text("No images to show."));
+              }
+              return GridView.builder(
+                  itemCount: box.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (BuildContext context, index) {
+                    Memory memory = box.getAt(index) as Memory;
+                    return Image.memory(memory.pictureBytes);
+                  });
+            }));
   }
 }
---
+++ lib/storage.dart
@@ -0,0 +1,46 @@
+import 'dart:typed_data';
+import 'package:hive/hive.dart';
+
+part 'storage.g.dart';
+
+@HiveType(typeId: 0)
+enum LifetimeTag {
+  @HiveField(0)
+  oneDay,
+  @HiveField(1)
+  sevenDays,
+  @HiveField(2)
+  thirtyDays,
+}
+
+const lifetimeTags = <LifetimeTag, String> {
+  LifetimeTag.oneDay: "1 Day",
+  LifetimeTag.sevenDays: "7 Days",
+  LifetimeTag.thirtyDays: "30 Days",
+};
+
+@HiveType(typeId: 1)
+class Memory {
+  /// The time the photo was created. This should not change once created.
+  @HiveField(0)
+  DateTime created;
+
+  /// Storing the image blob in the database.
+  @HiveField(1)
+  Uint8List pictureBytes;
+
+  /// The tag regarding the lifetime of the memory.
+  /// This can be updated by the user.
+  @HiveField(2)
+  LifetimeTag lifetimeTag;
+
+  /// Constructor
+  Memory(this.created, this.pictureBytes, this.lifetimeTag);
+}
+
+/// Generate adapters to convert Dart classes to ones stored in the Hive DB.
+void generateAdapters() {
+  Hive.registerAdapter<LifetimeTag>(LifetimeTagAdapter());
+  Hive.registerAdapter<Memory>(MemoryAdapter());
+
+}
