04fea34a4997215145f30cdcc925f8581f44bff6
FenyCz <FenyCz@github.com>
2022-11-15 15:16:02 +0100

    Change tag on photo detail

--- lib/photo_detail.dart
+++ lib/photo_detail.dart
@@ -12,6 +12,42 @@ import 'package:share_plus/share_plus.dart';
 
 import 'globals.dart';
 
+enum Menu { itemOne, itemTwo, itemThree }
+
+// TODO: where to put these map functions
+String mapToText(Menu menuItem) {
+  switch (menuItem) {
+    case Menu.itemOne:
+      return "1";
+    case Menu.itemTwo:
+      return "7";
+    case Menu.itemThree:
+      return "30";
+  }
+}
+
+Menu mapToMenu(LifetimeTag tag) {
+  switch (tag) {
+    case LifetimeTag.oneDay:
+      return Menu.itemOne;
+    case LifetimeTag.sevenDays:
+      return Menu.itemTwo;
+    case LifetimeTag.thirtyDays:
+      return Menu.itemThree;
+  }
+}
+
+LifetimeTag mapToTag(Menu menuItem) {
+  switch (menuItem) {
+    case Menu.itemOne:
+      return LifetimeTag.oneDay;
+    case Menu.itemTwo:
+      return LifetimeTag.sevenDays;
+    case Menu.itemThree:
+      return LifetimeTag.thirtyDays;
+  }
+}
+
 class PhotoDetail extends StatefulWidget {
   final Box<Memory> box;
   final int index;
@@ -20,76 +56,125 @@ class PhotoDetail extends StatefulWidget {
       : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => PhotoDetailState();
+  State<StatefulWidget> createState() => PhotoDetailState(curIndex: this.index);
 }
 
 class PhotoDetailState extends State<PhotoDetail> {
+  // default current is from PhotoDetail class
+  int curIndex;
+  PhotoDetailState({required this.curIndex});
+
+  // variables I don't want to change in build method with calling setState
+  Menu? _selectedMenu;
+  PhotoViewScaleStateController scaleStateController =
+      PhotoViewScaleStateController();
+
   @override
   Widget build(BuildContext context) {
-    // using page controller when you open first detail, instead of first photo you see choosen one
-    PageController _pageController = PageController(initialPage: widget.index);
-    Memory? memory = widget.box.getAt(widget.index);
+    // initial index is taken from PhotoDetail class
+    PageController _pageController = PageController(initialPage: curIndex);
+    // memory of current index
+    Memory? memory = widget.box.getAt(curIndex);
+    int popScreenCount = 0;
+
     return Scaffold(
         appBar: AppBar(
+          bottom: PreferredSize(
+              preferredSize: Size.fromHeight(5),
+              child: Container(
+                child: Text("Deleted in: ${memory!.getTimeToExpire()}"),
+              )),
           leading: IconButton(
               icon: const Icon(Icons.arrow_back),
               onPressed: () async {
                 Navigator.pop(context);
               }),
           actions: <Widget>[
-            // Rotating flash button
+            PopupMenuButton<Menu>(
+                onSelected: (Menu item) {
+                  setState(() {
+                    _selectedMenu = item;
+                    widget.box.getAt(curIndex)!.lifetimeTag = mapToTag(item);
+                  });
+                },
+                icon: Text(mapToText(mapToMenu(memory.lifetimeTag))),
+                iconSize: 60.0,
+                itemBuilder: (BuildContext context) => <PopupMenuEntry<Menu>>[
+                      const PopupMenuItem<Menu>(
+                        value: Menu.itemOne,
+                        child: Text('1 Day'),
+                      ),
+                      const PopupMenuItem<Menu>(
+                        value: Menu.itemTwo,
+                        child: Text('7 Days'),
+                      ),
+                      const PopupMenuItem<Menu>(
+                        value: Menu.itemThree,
+                        child: Text('30 Days'),
+                      ),
+                    ]),
             IconButton(
-              icon: const Icon(Icons.info),
-              onPressed: () async {
-                Navigator.pop(context);
-              },
-            ),
+                icon: const Icon(Icons.share),
+                onPressed: () async {
+                  final directory = (await getExternalStorageDirectory())?.path;
+                  File imgFile = new File('${directory}assets/screenshot.png');
+                  imgFile.writeAsBytes(memory!.pictureBytes);
+                  await Share.shareXFiles(
+                      [XFile('${directory}assets/screenshot.png')]);
+                  //TODO: check if deleted
+                  imgFile.delete();
+                }),
             IconButton(
-              icon: const Icon(Icons.share),
-              onPressed: () async {
-                final directory = (await getExternalStorageDirectory())?.path;
-                File imgFile = new File('${directory}assets/screenshot.png');
-                imgFile.writeAsBytes(memory!.pictureBytes);
-                await Share.shareXFiles(
-                    [XFile('${directory}assets/screenshot.png')],
-                    text: 'Look what I made.');
-
-                //TODO: delete photo that I store
-                //Navigator.pop(context);
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.delete),
-              onPressed: () async {
-                //TODO: delete from db
-                deleteMemory(memory!);
-                Navigator.pop(context);
-              },
-            )
+                icon: const Icon(Icons.delete),
+                onPressed: () {
+                  showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                      title: const Text('Do you want to delete this photo?'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () async {
+                            deleteMemory(memory!);
+                            // two pop
+                            Navigator.of(context)
+                                .popUntil((_) => popScreenCount++ >= 2);
+                            popScreenCount = 0;
+                          },
+                          child: const Text('Delete'),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, 'Cancel'),
+                          child: const Text('Cancel'),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
           ],
         ),
         body: Container(
             child: PhotoViewGallery.builder(
           scrollPhysics: const BouncingScrollPhysics(),
           pageController: _pageController,
+          onPageChanged: (int index) {
+            setState(() {
+              curIndex = index;
+            });
+          },
           builder: (BuildContext context, int index) {
             memory = widget.box.getAt(index);
-
-            // TODO: fix null memory
-            // if (memory == null) {
-            //   return PhotoViewGalleryPageOptions(
-            //       imageProvider: AssetImage("assets/images/1.jpg"));
-            // }
-
             return PhotoViewGalleryPageOptions(
-              imageProvider: MemoryImage(memory!.pictureBytes),
-              heroAttributes: PhotoViewHeroAttributes(tag: index),
-              maxScale: PhotoViewComputedScale.covered * 3,
-              minScale: PhotoViewComputedScale.contained * 0.8,
-            );
+                imageProvider: MemoryImage(memory!.pictureBytes),
+                heroAttributes: PhotoViewHeroAttributes(tag: curIndex),
+                maxScale: PhotoViewComputedScale.covered * 3,
+                minScale: PhotoViewComputedScale.contained,
+                scaleStateController: scaleStateController,
+                // TODO: TEST ME: rotation? only for the time you are touching the display
+                gestureDetectorBehavior: HitTestBehavior.deferToChild,
+                onScaleEnd: (context, details, controllerValue) =>
+                    scaleStateController.scaleState =
+                        PhotoViewScaleState.covering);
           },
-
-          // TODO: rotation? only for the time you are touching the display
           enableRotation: true,
           itemCount: widget.box.length,
           loadingBuilder: (context, event) => Center(
--- lib/storage/schema.dart
+++ lib/storage/schema.dart
@@ -14,14 +14,28 @@ enum LifetimeTag {
   thirtyDays,
 }
 
-const lifetimeTags = <LifetimeTag, String> {
+const lifetimeTags = <LifetimeTag, String>{
   LifetimeTag.oneDay: "1 Day",
   LifetimeTag.sevenDays: "7 Days",
   LifetimeTag.thirtyDays: "30 Days",
 };
 
+extension LifetimeTagExtension on LifetimeTag {
+  /// Convert a lifetime tag to Duration
+  Duration toDuration() {
+    switch (this) {
+      case LifetimeTag.oneDay:
+        return const Duration(days: 1);
+      case LifetimeTag.sevenDays:
+        return const Duration(days: 7);
+      case LifetimeTag.thirtyDays:
+        return const Duration(days: 30);
+    }
+  }
+}
+
 @HiveType(typeId: 1)
-class Memory {
+class Memory extends HiveObject {
   /// The time the photo was created. This should not change once created.
   @HiveField(0)
   DateTime created;
@@ -37,6 +51,29 @@ class Memory {
 
   /// Constructor
   Memory(this.created, this.pictureBytes, this.lifetimeTag);
+
+  /// Return the DateTime when the Memory will be considered expired
+  DateTime getExpiration() {
+    return created.add(lifetimeTag.toDuration());
+  }
+
+  String getTimeToExpire() {
+    Duration timeToExpire =
+        created.add(lifetimeTag.toDuration()).difference(DateTime.now());
+    if (timeToExpire.inDays == 0) {
+      return "${timeToExpire.inHours} hours";
+    } else {
+      return "${timeToExpire.inDays} days";
+    }
+  }
+
+  /// Check if the current Memory is expired.
+  bool isExpired() {
+    DateTime expirationTime = getExpiration();
+    DateTime currentTime = DateTime.now();
+
+    return currentTime.isAfter(expirationTime) ? true : false;
+  }
 }
 
 /// Generate adapters to convert Dart classes to ones stored in the Hive DB.
